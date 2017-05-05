c6b35ca0207cb84e784435536786e535baefbccb
Patricio G Maseda <pmaseda@sparkdigital.com>
2017-05-04 23:11:54 -0300

    Removing non-needed components

--- src/components/PageContent/PageContent.jsx
+++ src/components/PageContent/PageContent.jsx
@@ -1,9 +1,7 @@
 import React from 'react';
 
 // Components.
-import NewsletterComposer from '../NewsletterComposer/NewsletterComposer';
 import Preview from '../Preview/Preview';
-import ToolBar from '../ToolBar/ToolBar';
 
 const PageContent = (props) => (
   <div className="pageContent">
