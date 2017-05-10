658d66e492a791e7f3ea3f0dc000d647ddf0e3c1
Patricio G Maseda <pmaseda@sparkdigital.com>
2017-05-10 17:24:01 -0300

    Adapting layout to the preview image idea.

--- src/components/PageContent/PageContent.jsx
+++ src/components/PageContent/PageContent.jsx
@@ -2,11 +2,15 @@ import React from 'react';
 
 // Components.
 import List from '../List/List';
+import Preview from '../Preview/Preview';
 
 const PageContent = (props) => (
   <div className="pageContent">
     <h1>My Awesome Movies App</h1>
-    <List {...props}/>
+    <div className="pageContent-main">
+      <Preview />
+      <List {...props}/>
+    </div>
   </div>
 );
 
---
+++ src/components/Preview/Preview.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Preview = (props) => <div className="preview"></div>;
+
+export default Preview;
